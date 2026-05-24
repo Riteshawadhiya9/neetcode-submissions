a23b1b97ca1d746c6ae691bd08861fb0a240b8a2
Ritesh Awadhiya <riteshawadhiya2004@gmail.com>
2026-05-24 21:33:38 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+        int ans = -1;
+        int st = 0 , end = x;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            long val = (long) mid * mid;
+            if(val == x){
+                return mid;
+            }else if(val > x){
+                end = mid-1;
+            }else{
+                st = mid+1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+}
