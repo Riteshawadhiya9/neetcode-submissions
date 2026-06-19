33afd7cd5750127f870696f9c9c94419b6f25b8f
Ritesh Awadhiya <riteshawadhiya2004@gmail.com>
2026-06-19 21:34:34 +0530

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int ans = 0;
+        for(int i = 0 ; i < s.length()-1; i++){
+            char ch = s.charAt(i);
+            char ch1 = s.charAt(i+1);
+            ans += Math.abs((int)ch - (int)ch1);
+            // ans += Math.abs(ch - ch1); // both are same
+        }
+        return ans;
+    }
+}
